8ef869407f541fda433820e2d2953eabb19c12ba
Ali Chraghi <63465728+AliChraghi@users.noreply.github.com>
2021-06-04 00:56:35 +0430

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <loop/ast.hpp>
-#include <loop/console.hpp>
 #include <loop/global.hpp>
 #include <loop/io.hpp>
 #include <loop/tokenizer.hpp>
@@ -16,7 +15,7 @@ int main(int argc, const char *argv[]) {
             tokenizer.tokenize(io.reads[i].data);
 
             for (size_t i = 0; i < werror.errors.size(); i++) {
-                outerr << "\033[91;1mE" << std::setfill('0') << std::setw(2) << werror.errors[i].code
+                std::cerr << "\033[91;1mE" << std::setfill('0') << std::setw(2) << werror.errors[i].code
                        << " \033[0mIn \033[4m" << werror.errors[i].filePath << ':' << werror.errors[i].line << '\n'
                        << "\033[0;91;1mERROR\x1b[0;1m: " << werror.errors[i].message << '\n';
             }
@@ -26,7 +25,7 @@ int main(int argc, const char *argv[]) {
                 if (token.kind != T_NEWLINE) out << token.kind << ':' << token.value << '\n';
             }
 
-            out << "Tokenization was \033[92;1mSuccessful\033[0;1m (" << tokenizer.tokens.size() << " tokens found)"
+            std::cout << "Tokenization was \033[92;1mSuccessful\033[0;1m (" << tokenizer.tokens.size() << " tokens found)"
                 << '\n';
         }
     }
